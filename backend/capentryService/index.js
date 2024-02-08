9ffca7bd08211e2f2bc31bc529d1dba1570e0d46
Ndigithe-john <ndigithejohn@gmail.com>
2024-02-08 14:51:57 -0800

    app setting

---
+++ backend/capentryService/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
---
+++ backend/userAuthService/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+
+app.use(express.json());
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
