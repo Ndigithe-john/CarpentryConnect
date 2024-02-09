d4ace518ddbe62c97a92a5fe8b19738a69e68fd5
Ndigithe-john <ndigithejohn@gmail.com>
2024-02-09 10:12:54 -0800

    added the connection

--- backend/userAuthService/index.js
+++ backend/userAuthService/index.js
@@ -3,11 +3,22 @@ require("dotenv").config();
 const app = express();
 const sql = require("mssql");
 const config = require("./src/config/databaseConfig");
+const AppError = require("./src/utils/appError");
+const globalErrorHandlers = require("./src/controllers/errorControllers");
 app.use(express.json());
 async function startServer() {
+  const oneDay = 24 * 60 * 60 * 1000;
   try {
+    const pool = await sql.connect(config);
+    console.log(`connected to the database`);
+    app.all("*", (req, res, next) => {
+      next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+    });
+    app.use(globalErrorHandlers);
     const port = process.env.PORT || 4000;
     app.listen(port, () => console.log(`Server running on port ${port}`));
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+  }
 }
 startServer();
