717c502abd094516e7cdaa8bfb01aa12fdd79912
Ndigithe-john <ndigithejohn@gmail.com>
2024-02-09 11:10:08 -0800

    created a session storage for users

--- backend/userAuthService/index.js
+++ backend/userAuthService/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
+const session = require("express-session");
+const RedisStore = require("connect-redis").default;
+const { v4 } = require("uuid");
+const { createClient } = require("redis");
 const sql = require("mssql");
 const config = require("./src/config/databaseConfig");
 const AppError = require("./src/utils/appError");
@@ -8,9 +12,42 @@ const globalErrorHandlers = require("./src/controllers/errorControllers");
 app.use(express.json());
 async function startServer() {
   const oneDay = 24 * 60 * 60 * 1000;
+
   try {
     const pool = await sql.connect(config);
     console.log(`connected to the database`);
+
+    const redis_client = createClient();
+    redis_client.connect();
+    redis_client.on("connect", () => console.log("Connected to Redis"));
+    const redis_store = new RedisStore({ client: redis_client, prefix: "" });
+    app.use((req, res, next) => {
+      req.pool = pool;
+      next();
+    });
+    app.use(
+      session({
+        store: redis_store,
+        secret: process.env.SESSION_SECRET,
+        saveUninitialized: false,
+        genid: () => v4(),
+        resave: false,
+        rolling: true,
+        unset: "destroy",
+        cookie: {
+          httpOnly: true,
+          secure: false,
+          maxAge: oneDay,
+          domain: "localhost",
+        },
+      })
+    );
+    app.get("/", (req, res) => {
+      req.session.authorized = true;
+      req.session.user = "vic";
+      console.log(req.sessionID);
+      res.send("okay");
+    });
     app.all("*", (req, res, next) => {
       next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
     });
---
+++ backend/userAuthService/src/controllers/userControllers.js
@@ -0,0 +1,7 @@
+const AppError = require("../utils/appError");
+async function signUp(req, res) {
+  try {
+    let newUser = req.body;
+    const pool = req;
+  } catch (error) {}
+}
