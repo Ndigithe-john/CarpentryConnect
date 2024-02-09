72e31936433aaf4eae48bdfa23e37bbb12ce9e60
Ndigithe-john <ndigithejohn@gmail.com>
2024-02-09 09:40:52 -0800

    added Error controllers

--- backend/userAuthService/index.js
+++ backend/userAuthService/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
-
+const sql = require("mssql");
+const config = require("./src/config/databaseConfig");
 app.use(express.json());
-const port = process.env.PORT || 4000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+async function startServer() {
+  try {
+    const port = process.env.PORT || 4000;
+    app.listen(port, () => console.log(`Server running on port ${port}`));
+  } catch (error) {}
+}
+startServer();
