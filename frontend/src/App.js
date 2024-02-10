004c584108699c05a6efaf395d25b663b371677e
Ndigithe-john <ndigithejohn@gmail.com>
2024-02-10 05:35:54 -0800

    added the route path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,27 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <LandingPage />,
+    },
+    {
+      path: "/login",
+      element: <LoginPage />,
+    },
+    {
+      path: "/register",
+      element: <RegisterPage />,
+    },
+  ]);
   return (
-    <div>
-      <LoginPage />
-    </div>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
