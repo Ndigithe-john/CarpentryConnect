6fb851ab153b03f251bc0d77237ed1b86f1f4135
Ndigithe-john <ndigithejohn@gmail.com>
2024-02-14 10:39:02 +0300

    adding a modal

---
+++ frontend/src/components/AccountAboutModal.js
@@ -0,0 +1,11 @@
+const AccountAboutModal = () => {
+  return (
+    <div>
+      <h4>Account</h4>
+      <h4>Dashboard</h4>
+      <h4>logout</h4>
+    </div>
+  );
+};
+
+export default AccountAboutModal;
