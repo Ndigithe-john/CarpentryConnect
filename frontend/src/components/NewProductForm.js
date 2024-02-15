91ba25443527955b026d72aa7186d7629762be01
Ndigithe-john <ndigithejohn@gmail.com>
2024-02-15 14:13:43 +0300

    Form to add a product

--- frontend/src/components/NewProductForm.js
+++ frontend/src/components/NewProductForm.js
@@ -1,5 +1,43 @@
+import { useState } from "react";
 const NewProductForm = () => {
-  return <div></div>;
+  const [itemUpload, setItemUpload] = useState();
+  function handleItemUpload(e) {
+    setItemUpload(URL.createObjectURL(e.target.files[0]));
+  }
+  return (
+    <form>
+      <input type="file" onChange={handleItemUpload} />
+      <img src={itemUpload} alt="itemupload" />
+      <textarea rows="4" cols="50">
+        Enter item Description
+      </textarea>
+      <select>
+        <option>Seating</option>
+        <option>Sleeping</option>
+        <option>Storage</option>
+        <option>Dinning</option>
+        <option>Office</option>
+        <option>Outdoor</option>
+        <option>Accent</option>
+        <option>Industrial</option>
+        <option>Entryway</option>
+      </select>
+      <select>
+        <option>Oak</option>
+        <option>Maple</option>
+        <option>Hemlock</option>
+        <option>Mahogany</option>
+        <option>Ash</option>
+        <option>Cypress</option>
+        <option>Walnut</option>
+        <option>Spruce</option>
+        <option>Cedar</option>
+        <option>Cherry</option>
+      </select>
+      <input type="date" />
+      <input type="text" />
+    </form>
+  );
 };
 
 export default NewProductForm;
