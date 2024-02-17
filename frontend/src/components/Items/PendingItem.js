9632b9f5c5b1123a266e4ccf35163e25f02053cc
Ndigithe-john <ndigithejohn@gmail.com>
2024-02-17 14:37:39 +0300

    adding the item container

--- frontend/src/components/Items/PendingItem.js
+++ frontend/src/components/Items/PendingItem.js
@@ -1,7 +1,19 @@
 const PendingItem = ({ item }) => {
   return (
-    <div>
-      <img src={item.ImageURL} alt="pendingImage" />
+    <div className="pending_container_item">
+      <img src={item.ImageURL} alt="pendingImage" className="item_image" />
+      <div className="item_description">
+        <p>Item Description: {item.Description}</p>
+      </div>
+      <div className="capentryDetails">
+        <p>CapenterID:</p>
+        <p>CapenterName;</p>
+        <p>Qualification</p>
+        <div>
+          <button>reject</button>
+          <button>Approve</button>
+        </div>
+      </div>
     </div>
   );
 };
