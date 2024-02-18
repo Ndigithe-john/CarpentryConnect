fcb9bb01514b82900fe88578aeeae6aeb3a3fcc8
Ndigithe-john <ndigithejohn@gmail.com>
2024-02-18 18:20:42 +0300

    adding the app top page

---
+++ frontend/src/TopProduct/Category.js
@@ -0,0 +1,17 @@
+const Category = () => {
+  return (
+    <div className="category_aside_container">
+      <p>Seating</p>
+      <p>Sleeping</p>
+      <p>Storage</p>
+      <p>Dinning</p>
+      <p>Office</p>
+      <p>Outdoor</p>
+      <p>Accent</p>
+      <p>Industrialion</p>
+      <p>Entryway</p>
+    </div>
+  );
+};
+
+export default Category;
