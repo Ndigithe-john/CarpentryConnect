94e2ec8d254ad7dbc89c6a6a2f73132fa107297e
Ndigithe-john <ndigithejohn@gmail.com>
2024-02-19 08:53:35 +0300

    added the output of the topmost container

---
+++ frontend/src/TopProduct/DisplayContainer.js
@@ -0,0 +1,14 @@
+import Category from "./Category";
+import MainHeader from "./MainHeader";
+import RightSide from "./RightSide";
+const DisplayContainer = () => {
+  return (
+    <div>
+      <Category />
+      <MainHeader />
+      <RightSide />
+    </div>
+  );
+};
+
+export default DisplayContainer;
