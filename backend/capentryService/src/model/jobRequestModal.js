c083dbbeae0be163a068818fc4c1dae18a50570d
Ndigithe-john <ndigithejohn@gmail.com>
2024-02-24 16:07:37 +0300

    added the id validation

--- backend/capentryService/src/model/jobRequestModal.js
+++ backend/capentryService/src/model/jobRequestModal.js
@@ -1,6 +1,7 @@
 const joi = require("joi");
 
 const jobRequestSchema = joi.object({
+  CarpenterID: joi.required(),
   ItemID: joi.required(),
   EstimatedCompletionDate: joi.date().required(),
   AdditionalNotes: joi.string().optional(),
