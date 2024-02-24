927f141e75fa0fd135e0d636c18fe5669ac514a9
Ndigithe-john <ndigithejohn@gmail.com>
2024-02-25 01:40:14 +0300

    changed the trigger to approve the workshopTable

--- backend/capentryService/src/database/Triggers.sql
+++ backend/capentryService/src/database/Triggers.sql
@@ -12,6 +12,7 @@ BEGIN
 END;
 
 --Trigger to update the job status
+-- Assume you have the WorkshopItems table with a Status column
 CREATE TRIGGER UpdateJobStatus
 ON WorkRequests
 AFTER UPDATE
@@ -19,11 +20,17 @@ AS
 BEGIN
     IF UPDATE(Status)
     BEGIN
-        -
         UPDATE wr
         SET wr.Status = 'Rejected'
         FROM WorkRequests wr
         INNER JOIN inserted i ON wr.ItemID = i.ItemID
         WHERE i.Status = 'Approved' AND wr.RequestID <> i.RequestID;
+
+        UPDATE wi
+        SET wi.Status = 'Approved'
+        FROM WorkshopItems wi
+        INNER JOIN WorkRequests wr ON wi.ItemID = wr.ItemID
+        INNER JOIN inserted i ON wr.RequestID = i.RequestID
+        WHERE i.Status = 'Approved';
     END;
 END;
