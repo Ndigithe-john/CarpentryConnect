f26d82e01a131aecfc6cb7fabf5757156436283c
Ndigithe-john <ndigithejohn@gmail.com>
2024-02-28 16:18:59 +0300

    adding the form to update the profile

--- frontend/src/pages/UpdateProfile.js
+++ frontend/src/pages/UpdateProfile.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const UpdateProfile = () => {
-  return <div>UpdateProfile</div>;
+  return (
+    <form className="update_profile_details">
+      <label>Email</label>
+      <input type="email" />
+      <label>PhoneNumber</label>
+      <input type="text" />
+      <label>Bio</label>
+      <textarea name="description" rows="4" placeholder="Enter a description" />
+      <button>update</button>
+    </form>
+  );
 };
 
 export default UpdateProfile;
