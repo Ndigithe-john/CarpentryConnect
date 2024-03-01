cf8dc21d4dfab993c8d1a970ef378bb7c39537e6
Ndigithe-john <ndigithejohn@gmail.com>
2024-03-01 15:17:13 +0300

    added another component to handle carpenters posts

---
+++ frontend/src/components/CarpenterPost.js
@@ -0,0 +1,9 @@
+const CarpenterPost = ({ post }) => {
+  return (
+    <div className="carpenter_post">
+      <img src={post} alt="myPost" />
+    </div>
+  );
+};
+
+export default CarpenterPost;
---
+++ frontend/src/components/CarpenterPosts.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import CarpenterPost from "./CarpenterPost";
+const CarpenterPosts = () => {
+  const [myPersonalPosts, setMyPersonalPosts] = useState([]);
+  useEffect(() => {
+    async function getMyPosts() {
+      try {
+        let apiURL = "http://localhost:5050/users/userItems";
+        const response = await axios.get(apiURL, { withCredentials: true });
+        console.log(response);
+        setMyPersonalPosts(response.data);
+      } catch (error) {}
+    }
+    getMyPosts();
+  });
+  return (
+    <div>
+      {myPersonalPosts.map((post) => (
+        <CarpenterPost key={post.ItemID} post={post} />
+      ))}
+    </div>
+  );
+};
+
+export default CarpenterPosts;
