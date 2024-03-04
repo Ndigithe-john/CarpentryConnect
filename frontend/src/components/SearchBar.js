28d8ee0cefc75cd3e83c026a20206e1c25c579b4
Ndigithe-john <ndigithejohn@gmail.com>
2024-03-04 15:12:36 +0300

    handling the search input'

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
 const SearchBar = () => {
+  const [searchInput, setSearchInput] = useState("");
+  function handeSerchChange(e) {
+    e.preventDefault();
+    setSearchInput(e.target.value);
+  }
   return (
     <form className="search_form">
-      <input type="text" placeholder="search" />
+      <input
+        type="text"
+        placeholder="search"
+        value={searchInput}
+        onChange={handeSerchChange}
+      />
       <button>Search</button>
     </form>
   );
