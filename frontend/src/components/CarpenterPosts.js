8063c6bec58882f327ffc2cc56c7252130380f2a
Ndigithe-john <ndigithejohn@gmail.com>
2024-03-06 15:37:51 +0300

    added another path for a single item

--- frontend/src/components/CarpenterPosts.js
+++ frontend/src/components/CarpenterPosts.js
@@ -1,19 +1,27 @@
 import axios from "axios";
+import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import CarpenterPost from "./CarpenterPost";
+
 const CarpenterPosts = () => {
+  const { UserId } = useParams();
   const [myPersonalPosts, setMyPersonalPosts] = useState([]);
+
   useEffect(() => {
     async function getMyPosts() {
       try {
-        let apiURL = "http://localhost:5050/users/userItems";
+        let apiURL = `http://localhost:5050/users/carpenterPosts/${UserId}`;
         const response = await axios.get(apiURL, { withCredentials: true });
 
         setMyPersonalPosts(response.data.data);
-      } catch (error) {}
+      } catch (error) {
+        console.error("Error fetching Carpenter posts:", error);
+      }
     }
+
     getMyPosts();
-  }, []);
+  }, [UserId]);
+
   return (
     <div className="products">
       {myPersonalPosts.map((post) => (
