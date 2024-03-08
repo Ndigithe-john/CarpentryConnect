ec21ee8f61253a6604d06eeba27807205a7f3c97
Ndigithe-john <ndigithejohn@gmail.com>
2024-03-08 10:56:35 +0300

    adding the chat page styles

--- frontend/src/components/ChatRoom/Chat.js
+++ frontend/src/components/ChatRoom/Chat.js
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
+import ScrollToBottom from "react-scroll-to-bottom";
 import sendIcon from "../../assets/sendIcon.jpg";
 const Chat = ({ socket, userName, room }) => {
   const [currentMessage, setCurrentMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
   async function handleSendMessage() {
     if (currentMessage !== "") {
       const messageData = {
@@ -14,19 +16,41 @@ const Chat = ({ socket, userName, room }) => {
           new Date(Date.now()).getMinutes(),
       };
       await socket.emit("send_message", messageData);
+      setMessageList((list) => [...list, messageData]);
     }
   }
   useEffect(() => {
     socket.on("receive_message", (data) => {
-      console.log(data);
+      setMessageList((list) => [...list, data]);
     });
   }, [socket]);
   return (
-    <div className="chat_container">
+    <div className="chat-window">
       <div className="chat-header">
         <p>Live Chat</p>
       </div>
-      <div className="chat-body"></div>
+      <div className="chat-body">
+        <ScrollToBottom className="message-container">
+          {messageList.map((messageContent) => {
+            return (
+              <div
+                className="message"
+                id={userName === messageContent.author ? "you" : "other"}
+              >
+                <div>
+                  <div className="message-content">
+                    <p>{messageContent.message}</p>
+                  </div>
+                  <div className="message-meta">
+                    <p id="time">{messageContent.time}</p>
+                    <p id="author">{messageContent.author}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </ScrollToBottom>
+      </div>
       <div className="chat-footer">
         <input
           type="text"
