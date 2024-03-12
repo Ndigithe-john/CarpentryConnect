cd0c034cfe63e8ce089b13f513098f5a6e3bb12f
Ndigithe-john <ndigithejohn@gmail.com>
2024-03-12 10:20:11 +0300

    Server setup

--- backend/m-pesa/index.js
+++ backend/m-pesa/index.js
@@ -1,2 +1,16 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
+require("dotenv").config();
+
+async function startMpesaServer() {
+  app.get("/", (req, res) => {
+    res.send("Changing the world");
+  });
+
+  const port = process.env.PORT || 8001;
+
+  app.listen(port, () => console.log(`App running on port ${port}`));
+}
+
+startMpesaServer();
