5ae812f29330cca2e982a2a62f7df4f7b5444c48
Ndigithe-john <ndigithejohn@gmail.com>
2024-03-12 11:52:10 +0300

    getting the access token

--- backend/m-pesa/index.js
+++ backend/m-pesa/index.js
@@ -1,20 +1,43 @@
 const express = require("express");
 const app = express();
+const axios = require("axios");
 app.use(express.json());
 require("dotenv").config();
-
+const port = process.env.PORT || 8001;
+const consumerKey = process.env.CONSUMERKEY;
+const consumerSecret = process.env.CONSUMERSECRET;
 async function startMpesaServer() {
-  app.get("/", (req, res) => {
-    res.send("Changing the world");
-  });
+  try {
+    app.get("/", (req, res) => {
+      res.send("Changing the world");
+    });
 
-  const port = process.env.PORT || 8001;
+    app.get("/access_token", (req, res) => {
+      const url = `https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials`;
+      const auth =
+        "Basic " +
+        Buffer.from(consumerKey + ":" + consumerSecret).toString("base64");
+      const headers = {
+        Authorization: auth,
+      };
+      axios
+        .get(url, {
+          headers: headers,
+        })
+        .then((response) => {
+          let data = response.data;
+          res.send(data);
+        });
+    });
 
-  app.listen(port, (error, live) => {
-    if (error) {
-      console.log(error);
-    } else console.log(`App running on port ${port}`);
-  });
+    app.listen(port, (error, live) => {
+      if (error) {
+        console.log(error);
+      } else console.log(`App running on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 startMpesaServer();
