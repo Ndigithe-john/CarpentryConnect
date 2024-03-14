2320774650617e26a61bacdbbb32727e25e6a20a
Ndigithe-john <ndigithejohn@gmail.com>
2024-03-14 15:31:53 +0300

    routes change

--- backend/m-pesa/src/Routes/mpesaRoutes.js
+++ backend/m-pesa/src/Routes/mpesaRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { accessToken, lipa_na_mpesa } = require("../controllers/lipanaMpesa");
+const {
+  accessToken,
+  lipa_na_mpesa,
+  CallBack,
+} = require("../controllers/lipanaMpesa");
 
 router.get("/access_token", accessToken);
 router.post("/stk/push", accessToken, lipa_na_mpesa);
-
+router.post("/callback", CallBack);
 module.exports = router;
